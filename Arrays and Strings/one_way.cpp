7d3e03cc6916e50c96d3c1e6d497952ae8a1c0f3
harshita <harshitabetala7@gmail.com>
2022-08-20 20:37:29 +0530

    one_way

---
+++ Arrays and Strings/one_way.cpp
@@ -0,0 +1,35 @@
+// given two strings check if they have a single edit or not
+//three edits can be performed. insert,remove or replace a character
+#include<iostream>
+#include<string.h>
+using namespace std;
+bool _checkEdit(string a,string b){
+int arr[27];
+std::fill_n(arr,27,0);
+int counter=0; //for checking replacement
+
+for(int i=0;i<a.length();i++){
+    int pos=int(a[i])-'a';
+    arr[pos]++;
+}
+
+for(int i=0;i<b.length();i++){
+    int pos=int(b[i])-'a';
+    if(arr[pos]==0)
+    {
+        arr[pos]++;
+        counter++;
+    }
+    if(counter>1)
+     return false;   
+}
+
+  return true;  
+}
+int main(){
+    string a,b;
+    cin>>a>>b;
+    _checkEdit(a,b)==true?cout<<"True":cout<<"False";
+
+    return 0;
+}
