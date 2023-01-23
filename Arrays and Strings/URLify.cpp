4ca6603543773dff5fda610a6636ee5d439270d2
Harshita Betala <harshitabetala@Harshitas-MacBook-Pro.local>
2023-01-24 01:39:01 +0530

    Solved using string stream

--- Arrays and Strings/URLify.cpp
+++ Arrays and Strings/URLify.cpp
@@ -3,26 +3,41 @@
 
 #include<iostream>
 #include<string>
-using namespace std;
-string URLify(string a){
-    string result;
-    for(int i=0;i<=a.length();i++){
-        if(a[i]==' '||a[i]=='\0')
-        {
-           result.append("%20");
+#include <sstream>
 
-        }
-        else{
-          result.append(1,a[i]);
-        }
-    }
-return result;
+using namespace std;
+
+//adding character by character
+// string URLify(string a){
+//     string result;
+//     for(int i=0;i<=a.length();i++){
+//         if(a[i]==' ')  //||a[i]=='\0'
+//         {
+//            result.append("%20");
+
+//         }
+//         else{
+//           result.append(1,a[i]);
+//         }
+//     }
+// return result;
+// }
+
+string URLify(string str){
+stringstream ss(str);
+string word,result;
+while(ss>>word){
+ result.append(word);
+ result.append("%20");
+}
+
+return result.erase(result.length()-3);
 }
 
 
 int main(){
 string input;
 getline(cin,input);
-cout<<URLify(input);
+cout<<URLify(input)<<" ";
 return 0;
 }
